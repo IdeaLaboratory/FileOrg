1cf149f410dd49700f9c9ba78098dad45a6ed603
AdiTCG <adithecomputerguy@gmail.com>
2016-04-09 04:13:18 +0530

    Added basic functionalitis and optio to move files instantly or periodically.

--- FileOrg/MainWindow.xaml.cs
+++ FileOrg/MainWindow.xaml.cs
@@ -26,6 +26,71 @@ namespace FileOrg
         public MainWindow()
         {
             InitializeComponent();
+
+            //code
+            //string source = @"F:\S", destination = @"F:\D";
+        }
+
+        private void MoveFiles(string source, string destination)
+        {
+            //get files that are need to move
+            IEnumerable<string> sFiles = Directory.GetFiles(source);
+            foreach (var item in sFiles)
+            {
+                string destFileFullPath = System.IO.Path.Combine(destination, System.IO.Path.GetFileName(item));
+                File.Move(item, destFileFullPath);
+            }
+        }
+
+        private void sourceButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                sourceTB.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void DestinationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DestinationTB.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Is Periodically operation?
+            bool? isPeriodic = PeriodicallyRB.IsChecked;
+            if (isPeriodic == true)
+            {
+                //TODO: Validate inpute
+                //Timer myTimer = new Timer();
+                //myTimer.Elapsed += new ElapsedEventHandler(IntervalOperation);
+                DispatcherTimer time = new DispatcherTimer();
+
+                double inputTimeInterval = 1;
+                double.TryParse(IntervalTB.Text, out inputTimeInterval);
+                time.Interval = TimeSpan.FromMinutes(inputTimeInterval);
+                time.Start();
+                time.Tick += IntervalOperation;
+            }
+            else
+            {
+                MoveFiles(sourceTB.Text, DestinationTB.Text);
+            }
+        }
+
+        /// <summary>
+        /// Operation need to perform in time interval
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        private void IntervalOperation(object source, EventArgs e)
+        {
+            MoveFiles(sourceTB.Text, DestinationTB.Text);
         }
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ File organizer is a tool for basic file operation with little smartness.
 
 #1
 In this version we will add a wpf app created using visual studio without any changes. so that it become easy to keep track change at each phase.
+
+#2
+Added basic functionalitis and optio to move files instantly or periodically.
